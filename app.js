d7e339bb181f35e162c29543fc9463722c262169
LouisChen-TW <yue841206@gmail.com>
2022-03-03 12:10:00 +0800

    refactor: modify mongoose config

--- app.js
+++ app.js
@@ -1,22 +1,12 @@
 const express = require("express");
 const exphbs = require("express-handlebars");
-const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const methodOverride = require("method-override");
 const routes = require("./routes");
+require("./config/mongoose");
+
 const app = express();
 const port = 3000;
-
-mongoose.connect("mongodb://localhost/restaurant-list"); // 設定連線到 mongoDB
-const db = mongoose.connection; // 取得資料庫連線狀態
-// 連線異常
-db.on("error", () => {
-  console.log("mongodb error!");
-});
-// 連線成功
-db.once("open", () => {
-  console.log("mongodb connected!");
-});
 
 // 用 app.use 規定每一筆請求都需要透過 body-parser 進行前置處理
 app.use(bodyParser.urlencoded({ extended: true }));
--- models/seeds/restaurantSeeder.js
+++ models/seeds/restaurantSeeder.js
@@ -1,18 +1,8 @@
-const mongoose = require("mongoose");
+const db = require("../../config/mongoose");
 const Restaurant = require("../restaurant.js");
 const restaurant = require("../../restaurant.json").results;
 
-mongoose.connect("mongodb://localhost/restaurant-list");
-
-const db = mongoose.connection;
-
-db.on("error", () => {
-  console.log("mongodb error!");
-});
-
 db.once("open", () => {
-  console.log("mongodb connected!");
-
   Restaurant.create(restaurant);
 
   console.log("done.");
