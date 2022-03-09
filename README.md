cad9e727150bac4564af345f6cf27da56416c728
LouisChen-TW <yue841206@gmail.com>
2022-03-09 13:27:32 +0800

    refactor: sorting page

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 ---
 
-![](demo.png)
+![](./public/demo.png)
 
 ---
 
@@ -40,14 +40,14 @@
 1. 請先確認有安裝 node.js 與 npm
 2. 將專案 clone 到本地
 3. 在本地開啟之後，透過終端機進入資料夾，輸入：
-   ```npm install```
+   `npm install`
 
 4. 安裝完畢後，繼續輸入：
-   ```npm run start```
+   `npm run start`
 5. 若看見此行訊息則代表順利運行，打開瀏覽器進入到以下網址
-   ```Listening on http://localhost:3000```
+   `Listening on http://localhost:3000`
 6. 若欲暫停使用
-   ```ctrl + c```
+   `ctrl + c`
 
 ### 開發工具
 
--- app.js
+++ app.js
@@ -1,29 +1,29 @@
-const express = require("express");
-const exphbs = require("express-handlebars");
-const bodyParser = require("body-parser");
-const methodOverride = require("method-override");
-const routes = require("./routes");
-require("./config/mongoose");
+const express = require('express')
+const exphbs = require('express-handlebars')
+const bodyParser = require('body-parser')
+const methodOverride = require('method-override')
+const routes = require('./routes')
+require('./config/mongoose')
 
-const app = express();
-const port = 3000;
+const app = express()
+const port = 3000
 
 // 用 app.use 規定每一筆請求都需要透過 body-parser 進行前置處理
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: true }))
 
 // express template engine
-app.engine("handlebars", exphbs({ defaultLayout: "main" }));
-app.set("view engine", "handlebars");
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
+app.set('view engine', 'handlebars')
 
 // setting static files
-app.use(express.static("public"));
+app.use(express.static('public'))
 
 // use methodoverride
-app.use(methodOverride("_method"));
+app.use(methodOverride('_method'))
 
 // 將 request 導入路由器
-app.use(routes);
+app.use(routes)
 
 app.listen(port, () => {
-  console.log(`Express is listening on http://localhost:${port}`);
-});
+  console.log(`Express is listening on http://localhost:${port}`)
+})
