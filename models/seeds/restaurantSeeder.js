8c60ba38c9629037aa23fda3579c5e8b868aa1bc
LouisChen-TW <yue841206@gmail.com>
2022-03-10 14:11:17 +0800

    modify seeder.js, will automatically exit after run npm

--- models/seeds/restaurantSeeder.js
+++ models/seeds/restaurantSeeder.js
@@ -4,6 +4,10 @@ const restaurant = require('../../restaurant.json').results
 
 db.once('open', () => {
   Restaurant.create(restaurant)
-
-  console.log('done.')
+    .then(() => {
+      console.log('done.')
+      db.close()
+    })
+    .catch((error) => console.log(error))
+    .finally(() => process.exit())
 })
