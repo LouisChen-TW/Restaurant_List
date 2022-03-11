dcf0f5e7bbadc1e6e0c3131e46539c944a0bca46
LouisChen-TW <yue841206@gmail.com>
2022-03-11 10:17:47 +0800

    modify form validation

--- public/javascripts/validate.js
+++ public/javascripts/validate.js
@@ -1,10 +1,11 @@
 // const submitButton = document.querySelector('#submitButton')
 const form = document.querySelector('#form')
+const imgUrl = document.querySelector('#image')
 
 form.addEventListener(
   'submit',
   function (event) {
-    if (!form.checkValidity()) {
+    if (!form.checkValidity() || !isImage(imgUrl.value)) {
       event.preventDefault()
       event.stopPropagation()
     }
@@ -12,3 +13,7 @@ form.addEventListener(
   },
   false
 )
+
+function isImage(url) {
+  return /\.(jpg|jpeg|png|webp|avif|gif|svg)$/.test(url)
+}
