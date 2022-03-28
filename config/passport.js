1257c20655f4ae112271e5e1ba28428f90e9a808
LouisChen-TW <yue841206@gmail.com>
2022-03-28 14:13:33 +0800

    feat: add bcrypt in register and login

--- config/passport.js
+++ config/passport.js
@@ -1,5 +1,6 @@
 const passport = require('passport')
 const LocalStrategy = require('passport-local').Strategy
+const bcrypt = require('bcryptjs')
 const User = require('../models/user')
 
 module.exports = (app) => {
@@ -16,10 +17,12 @@ module.exports = (app) => {
             if (!user) {
               return done(null, false, { message: '此信箱尚未註冊' })
             }
-            if (user.password !== password) {
-              return done(null, false, { message: '信箱或是密碼錯誤' })
-            }
-            return done(null, user)
+            return bcrypt.compare(password, user.password).then((isMatch) => {
+              if (!isMatch) {
+                return done(null, false, { message: '信箱或是密碼錯誤' })
+              }
+              return done(null, user)
+            })
           })
           .catch((err) => done(err, false))
       }
